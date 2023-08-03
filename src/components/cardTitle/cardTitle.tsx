ea1657d03480f3b80499beac25849481bf15ce0a
Christian Kongsgaard <chrk@arkitema.com>
2023-08-03 16:35:30 +0200

    updated CardTitle to take custom button

--- src/components/cardTitle/cardTitle.tsx
+++ src/components/cardTitle/cardTitle.tsx
@@ -7,13 +7,14 @@ interface CardTitleProps extends TypographyProps {
   title: string
   size: 'large' | 'medium' | 'small'
   onClickHandler?: (e: React.MouseEvent<HTMLElement>) => void
+  customButton?: React.ReactNode
   'data-testid'?: string
   disableButton?: boolean
   tooltipText?: string
 }
 
 export const CardTitle = (props: CardTitleProps) => {
-  const { title, size, onClickHandler, tooltipText, disableButton } = props
+  const { title, size, onClickHandler, tooltipText, disableButton, customButton } = props
   let buttonSize
   let variant: 'h1' | 'h3' | 'h6'
   // Small to large font increase by size * 1,1 per size category
@@ -38,35 +39,38 @@ export const CardTitle = (props: CardTitleProps) => {
       variant = 'h6'
       break
   }
+
+  const button = customButton || (
+    <IconButton
+      data-testid='add-icon-button'
+      onClick={onClickHandler}
+      disabled={disableButton}
+      sx={{
+        padding: 'unset',
+        marginLeft: '10px',
+        width: `${buttonSize * 1.5}px`,
+        height: `${buttonSize * 1.5}px`,
+      }}
+    >
+      <AddIcon
+        sx={{
+          boxShadow: '0px 1px 2px #00000061',
+          fill: '#333',
+          borderRadius: '100%',
+          height: `${buttonSize}px`,
+          width: `${buttonSize}px`,
+          padding: `${buttonSize / 4}px`,
+        }}
+      />
+    </IconButton>
+  )
+
   return (
     <Typography variant={variant} data-testid={props['data-testid']}>
       {title}
       {onClickHandler && (
         <Tooltip title={tooltipText}>
-          <span>
-            <IconButton
-              data-testid='add-icon-button'
-              onClick={onClickHandler}
-              disabled={disableButton}
-              sx={{
-                padding: 'unset',
-                marginLeft: '10px',
-                width: `${buttonSize * 1.5}px`,
-                height: `${buttonSize * 1.5}px`,
-              }}
-            >
-              <AddIcon
-                sx={{
-                  boxShadow: '0px 1px 2px #00000061',
-                  fill: '#333',
-                  borderRadius: '100%',
-                  height: `${buttonSize}px`,
-                  width: `${buttonSize}px`,
-                  padding: `${buttonSize / 4}px`,
-                }}
-              />
-            </IconButton>
-          </span>
+          <span>{button}</span>
         </Tooltip>
       )}
     </Typography>
